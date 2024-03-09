9eac653eebf9194b72070f0e4b3aa609aa3358d9
giorgigvritishvili <gvritishvilig720@gmail.com>
2024-03-09 05:36:46 -0800

    first commit

--- day11/day11.py
+++
@@ -1,6 +0,0 @@
-mamis_asaki=int(input("შეიყვანე დედის ასაკი"))
-dedis_asaki=int(input("შეიყვანე მამის ასაკი"))
-if mamis_asaki>dedis_asaki:
-    print("მამა დიდია დედზე")
-if dedis_asaki>mamis_asaki:
-    print("დედა დიდია მამაზე")
