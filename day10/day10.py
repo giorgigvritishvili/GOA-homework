61ea9057f0a0d9a8cd403259f3579b80b3d1a377
giorgigvritishvili <gvritishvilig720@gmail.com>
2024-03-09 05:32:43 -0800

    first commit

--- day10/day10.py
+++
@@ -1,5 +0,0 @@
-bool1=int(input("რამდენი წლის ხარ?:   "))
-bool2=int(input("რამდენი გოლია გატანილი?:   "))
-bool1=32>21
-bool2 =21>32
-print(bool1 and bool2)
---
+++ day11/day11.py
@@ -0,0 +1,6 @@
+mamis_asaki=int(input("შეიყვანე დედის ასაკი"))
+dedis_asaki=int(input("შეიყვანე მამის ასაკი"))
+if mamis_asaki>dedis_asaki:
+    print("მამა დიდია დედზე")
+if dedis_asaki>mamis_asaki:
+    print("დედა დიდია მამაზე")
