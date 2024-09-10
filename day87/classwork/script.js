9d0807263075476dc68d1765b4e8729aed0efe85
giorgigvritishvili <158842639+giorgigvritishvili@users.noreply.github.com>
2024-09-10 14:07:59 +0400

    Add files via upload

---
+++ day87/classwork/script.js
@@ -0,0 +1,15 @@
+let minute =0
+const id = setInterval (function(){
+minute++;
+console.log("minute:" + time)
+
+if (minute == 34);{
+    console.log("Time up")
+    clearInterval(Date);
+
+
+}
+    
+console.log(Date)
+
+}, 60000 )
