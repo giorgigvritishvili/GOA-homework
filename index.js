53dadf9f3baad3350b702b6df526d26e6511b81d
giorgigvritishvili <gvritishvilig720@gmail.com>
2024-11-13 21:52:19 +0400

    commit

--- index.js
+++
@@ -1,2 +0,0 @@
-import {randomSuperhero} from 'superheroes';
-console.log(randomSuperhero())
--- scripr.js
+++
@@ -1,3 +0,0 @@
-fetch("https://pokeapi.co/api/v2/pokemon/ditto")
-        .then(response => console.log(response))
-        .cetch(error => console.error(error));
